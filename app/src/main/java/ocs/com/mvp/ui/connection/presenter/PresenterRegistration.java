41a833ef9b0c57317e4ee2025da71c480f6618c5
Rajasekaran M <rajasekaran.m@oclocksoftware.com>
2017-11-10 21:29:38 +0530

    # renamed variables and updated git ignore

--- app/src/main/java/ocs/com/mvp/ui/connection/presenter/PresenterRegistration.java
+++ app/src/main/java/ocs/com/mvp/ui/connection/presenter/PresenterRegistration.java
@@ -4,20 +4,17 @@ package ocs.com.mvp.ui.connection.presenter;
  */
 
 import android.content.Context;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
 
 
 import ocs.com.mvp.databinding.ActivityRegistrationBinding;
 import ocs.com.mvp.ui.connection.contractor.IContractorRegistration;
 
 public class PresenterRegistration implements IContractorRegistration.IRegistrationPresenter {
-    private IContractorRegistration.IRegistrationView iMainView;
+    private IContractorRegistration.IRegistrationView iRegistrationView;
     private Context context;
 
-    public PresenterRegistration(IContractorRegistration.IRegistrationView iMainView, Context context) {
-        this.iMainView = iMainView;
+    public PresenterRegistration(IContractorRegistration.IRegistrationView iRegistrationView, Context context) {
+        this.iRegistrationView = iRegistrationView;
         this.context = context;
     }
 
@@ -25,7 +22,7 @@ public class PresenterRegistration implements IContractorRegistration.IRegistrat
     public void launchRegisterAPI(ActivityRegistrationBinding binding) {
         if (areFieldsValid(binding)) {
             //write your business logic for api call
-           iMainView.showAPIRegisterSuccess();
+           iRegistrationView.showAPIRegisterSuccess();
         }
     }
 
